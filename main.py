4e625881e52077ff2455ec12f03d78495051370c
Ansire <ansiresu@gmail.com>
2018-04-23 21:24:21 +0800

    name of the optimizer

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ parser.add_argument('--dataset_name', dest='dataset_name', default='4legs', help
 parser.add_argument('--epoch', dest='epoch', type=int, default=150, help='# of epoch')
 parser.add_argument('--batch_size', dest='batch_size', type=int, default=1, help='# images in batch')
 parser.add_argument('--output_size', dest='output_size', type=int, default=256, help='then crop to this size')
-parser.add_argument('--lr', dest='lr', type=float, default=5e-5, help='initial learning rate for adam')
+parser.add_argument('--lr', dest='lr', type=float, default=5e-5, help='initial learning rate for optimizer')
 parser.add_argument('--phase', dest='phase', default='train', help='train, test')
 parser.add_argument('--checkpoint_dir', dest='checkpoint_dir', default='./checkpoint', help='models are saved here')
 parser.add_argument('--sample_dir', dest='sample_dir', default='./sample', help='sample are saved here')
